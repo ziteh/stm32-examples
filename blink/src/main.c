303312328fcdcfa24b211aeeaf840d365773c7c6
ziteh <honmonoh@gmail.com>
2021-11-14 13:39:50 +0800

    Add more comment for 'blink'

--- blink/src/main.c
+++ blink/src/main.c
@@ -1,45 +1,41 @@
 /**
  * @file   main.c
  * @author ZiTe (honmonoh@gmail.com)
- * @brief  Basic LED blinking example.
+ * @brief  Blinking LED example.
  */
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-// The User-LED is PA5(Port-A, Pin-5).
+/* The User-LED was on PA5 (Port-A, Pin-5) in my case (NUCLEO-F103RB). */
 #define RCC_LED_PORT (RCC_GPIOA)
-#define LED_PORT (GPIOA)
-#define LED_PIN (GPIO5)
-
-void gpio_setup(void)
-{
-  // Enable RCC.
-  rcc_periph_clock_enable(RCC_LED_PORT);
-
-  // Setup GPIO mode.
-  gpio_set_mode(LED_PORT,
-                GPIO_MODE_OUTPUT_2_MHZ,
-                GPIO_CNF_OUTPUT_PUSHPULL,
-                LED_PIN);
-}
+#define LED_PORT     (GPIOA)
+#define LED_PIN      (GPIO5)
 
 void delay(int value)
 {
-  for (int i = 0; i < value; i++)
+  while(value--)
   {
-    __asm__("nop");
+    __asm__("nop"); /* Do nothing. */
   }
 }
 
 int main(void)
 {
-  gpio_setup();
+  /* Enable clock. */
+  rcc_periph_clock_enable(RCC_LED_PORT);
 
+  /* Configure GPIO as push-pull output and maximum speed of 2 MHz. */
+  gpio_set_mode(LED_PORT,
+                GPIO_MODE_OUTPUT_2_MHZ,
+                GPIO_CNF_OUTPUT_PUSHPULL,
+                LED_PIN);
+
+  /* Start blinking. */
   while (1)
   {
-    gpio_toggle(LED_PORT, LED_PIN);
-    delay(1000000);
+    gpio_toggle(LED_PORT, LED_PIN); /* LED on/off. */
+    delay(1000000);                 /* Wait a bit. */
   }
 
   return 0;
