28470145a6aa567247db1948de6d7864b0f9a437
ZiTe <honmonoh@gmail.com>
2022-08-30 13:26:49 +0800

    Update usart_print

--- libopencm3/usart_printf/src/main.c
+++ libopencm3/usart_printf/src/main.c
@@ -1,41 +1,58 @@
 /**
  * @file   main.c
- * @author ZiTe (honmonoh@gmail.com)
- * @brief  USART with printf() function.
+ * @brief  USART with printf() function for STM32 Nucleo-F103RB and F446RE.
  * @remark Reference: https://github.com/libopencm3/libopencm3-examples/blob/master/examples/stm32/f1/stm32-h103/usart_printf/usart_printf.c
  */
 
 #include <stdio.h>
+#include <errno.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
-#include "printf.h"
 
-#define USART_BAUD_RATE (9600)
+#define USART_BAUDRATE (9600)
 
-void gpio_setup(void)
+#ifdef NUCLEO_F103RB
+  #define RCC_USART (RCC_USART2)
+  #define RCC_USART_TX_PORT (RCC_GPIOA)
+  #define GPIO_USART_TX_PORT (GPIOA)
+  #define GPIO_USART_TX_PIN (GPIO2)
+#elif NUCLEO_F446RE
+  #define RCC_USART (RCC_USART2)
+  #define RCC_USART_TX_PORT (RCC_GPIOA)
+  #define GPIO_USART_TX_PORT (GPIOA)
+  #define GPIO_USART_TX_PIN (GPIO2)
+#else
+  #error
+#endif
+
+void rcc_setup(void)
 {
-  rcc_periph_clock_enable(RCC_GPIOA);
-
-  /* Setup Tx pin. */
-  gpio_set_mode(GPIOA,
-                GPIO_MODE_OUTPUT_50_MHZ,
-                GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
-                GPIO2);
-
-  /* Setup User-LED. */
-  gpio_set_mode(GPIOA,
-                GPIO_MODE_OUTPUT_2_MHZ,
-                GPIO_CNF_OUTPUT_PUSHPULL,
-                GPIO5);
+  rcc_periph_clock_enable(RCC_USART_TX_PORT);
+  rcc_periph_clock_enable(RCC_USART);
 }
 
 void usart_setup(void)
 {
-  rcc_periph_clock_enable(RCC_USART2);
+  /* Set to output alternate function. */
+#ifdef NUCLEO_F103RB
+  gpio_set_mode(GPIOA,
+                GPIO_MODE_OUTPUT_50_MHZ,
+                GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                GPIO2);
+#else
+  gpio_mode_setup(GPIO_USART_TX_PORT,
+                  GPIO_MODE_AF,
+                  GPIO_PUPD_NONE,
+                  GPIO_USART_TX_PIN);
+
+  gpio_set_af(GPIO_USART_TX_PORT,
+              GPIO_AF7,
+              GPIO_USART_TX_PIN);
+#endif
 
   /* Setup USART config. */
-  usart_set_baudrate(USART2, USART_BAUD_RATE);
+  usart_set_baudrate(USART2, USART_BAUDRATE);
   usart_set_databits(USART2, 8);
   usart_set_stopbits(USART2, USART_STOPBITS_1);
   usart_set_parity(USART2, USART_PARITY_NONE);
@@ -45,27 +62,40 @@ void usart_setup(void)
   usart_enable(USART2);
 }
 
-void delay(int value)
+void delay(uint32_t value)
 {
   while (value--)
   {
-    __asm__("nop");
+    __asm__("nop"); /* Do nothing. */
   }
+}
+
+int _write(int file, char *ptr, int len)
+{
+  int i;
+
+  if (file == 1)
+  {
+    for (i = 0; i < len; i++)
+    {
+      usart_send_blocking(USART2, ptr[i]);
+    }
+    return i;
+  }
+
+  errno = EIO;
+  return -1;
 }
 
 int main(void)
 {
-  gpio_setup();
+  rcc_setup();
   usart_setup();
 
-  uint32_t i = 0;
-
+  int i = 0;
   while (1)
   {
-    printf("Hello World! %i\r\n", i);
-    gpio_toggle(GPIOA, GPIO5);
-    i++;
-
+    printf("Hello World! %i\r\n", i++);
     delay(500000);
   }
 
