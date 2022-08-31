7ffe0cf8d7d96416049e90d381613b32fe16e1d2
ZiTe <honmonoh@gmail.com>
2022-08-31 12:23:48 +0800

    Create PIO project for SPI master

---
+++ libopencm3/spi_master/src/main.c
@@ -0,0 +1,14 @@
+/**
+ * @file   main.c
+ * @author ZiTe (honmonoh@gmail.com)
+ * @brief  SPI master mode example for STM32 Nucleo-F103RB and F446RE.
+ */
+
+int main(void)
+{
+  while (1)
+  {
+  }
+
+  return 0;
+}
