9e3d323c5e4e40584edf661659d4df278a8c72c0
ziteh <honmonoh@gmail.com>
2022-09-03 22:10:27 +0800

    Add comment

--- libopencm3/pwm/src/main.c
+++ libopencm3/pwm/src/main.c
@@ -15,14 +15,14 @@
 
 /* If apb_presc = /1 than timer_clock = apb_clock, else timer_clock = 2* apb_clock. */
 #define PWM_TIMER_CLOCK (rcc_apb1_frequency * 2)
-#define PWM_TIMER_PRESCALER (PWM_TIMER_CLOCK / 1000000 - 1)
+#define PWM_TIMER_PRESCALER (PWM_TIMER_CLOCK / 1000000 - 1) /* TIMx_PSC value. */
 
 /*
  * f_goal = f_timer / [(Prescaler + 1) * (Period + 1)]
  * so
  * Period = {f_timer / [(Prescaler + 1) * f_goal]} - 1
  */
-#define PWM_TIMER_PERIOD (((PWM_TIMER_CLOCK) / ((PWM_TIMER_PRESCALER + 1) * PWM_GOAL_FREQUENCY)) - 1)
+#define PWM_TIMER_PERIOD (((PWM_TIMER_CLOCK) / ((PWM_TIMER_PRESCALER + 1) * PWM_GOAL_FREQUENCY)) - 1) /* TIMx_ARR value. */
 
 #if defined(NUCLEO_F103RB)
   #define RCC_PWM_GPIO (RCC_GPIOA)
@@ -51,7 +51,7 @@ static void rcc_setup(void)
 
 static void pwm_setup(void)
 {
-  /* Set PWM pin to alternate function. */
+  /* Set PWM pin to alternate function push-pull. */
 #if defined(NUCLEO_F103RB)
   gpio_set_mode(GPIO_PWM_PORT,
                 GPIO_MODE_OUTPUT_50_MHZ,
@@ -59,21 +59,19 @@ static void pwm_setup(void)
                 GPIO_PWM_PIN);
 #else
   gpio_mode_setup(GPIO_PWM_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PWM_PIN);
+  gpio_set_output_options(GPIO_PWM_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PWM_PIN);
   gpio_set_af(GPIO_PWM_PORT, GPIO_PWM_AF, GPIO_PWM_PIN);
 #endif
 
-  timer_set_mode(TIM3,
-                 TIM_CR1_CKD_CK_INT,
-                 TIM_CR1_CMS_EDGE,
-                 TIM_CR1_DIR_UP);
+  timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
   timer_disable_preload(TIM3);
   timer_continuous_mode(TIM3);
-  timer_set_prescaler(TIM3, PWM_TIMER_PRESCALER);
-  timer_set_period(TIM3, PWM_TIMER_PERIOD);
+  timer_set_prescaler(TIM3, PWM_TIMER_PRESCALER); /* Setup TIMx_PSC register. */
+  timer_set_period(TIM3, PWM_TIMER_PERIOD);       /* Setup TIMx_ARR register. */
   timer_set_oc_mode(TIM3, TIM_OC2, TIM_OCM_PWM1);
 
   uint32_t ccr_value = PWM_TIMER_PERIOD * PWM_GOAL_DUTY_CYCLE / 100.0;
-  timer_set_oc_value(TIM3, TIM_OC2, ccr_value);
+  timer_set_oc_value(TIM3, TIM_OC2, ccr_value); /* Setup TIMx_CCR register. */
 
   timer_enable_oc_output(TIM3, TIM_OC2);
   timer_enable_counter(TIM3);
