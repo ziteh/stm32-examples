b3ff3c1ddccd1225c0b45df5a00caad650616d70
ZiTe <honmonoh@gmail.com>
2022-09-28 16:34:02 +0800

    Change to injected

--- libopencm3/adc_multi_channel/src/main.h
+++ libopencm3/adc_multi_channel/src/main.h
@@ -1,6 +1,6 @@
 /**
  * @file   main.h
- * @brief  Multi channel ADC example for STM32 based on LibOpenCM3.
+ * @brief  ADC injected multi channel example for LibOpenCM3 with STM32.
  * @author ZiTe (honmonoh@gmail.com)
  * @copyright MIT License
  */
@@ -13,8 +13,8 @@ extern "C"
 {
 #endif
 
-#include <stdio.h>
-#include <errno.h>
+#include <stdio.h> /* For printf(). */
+#include <errno.h> /* For printf(). */
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/adc.h>
@@ -26,43 +26,32 @@ extern "C"
   #define ADC_SIMPLE_TIME (ADC_SMPR_SMP_55DOT5CYC)
   #define RCC_ADC_GPIO (RCC_GPIOA)
   #define GPIO_ADC_PORT (GPIOA)
-  #define GPIO_ADC_A0_PIN (GPIO0) /* A0. */
-  #define GPIO_ADC_A1_PIN (GPIO1) /* A1. */
-  #define GPIO_ADC_A2_PIN (GPIO4) /* A2. */
+  #define GPIO_ADC_IN0_PIN (GPIO0) /* Arduino-A0. */
+  #define GPIO_ADC_IN1_PIN (GPIO1) /* Arduino-A1. */
+  #define GPIO_ADC_IN4_PIN (GPIO4) /* Arduino-A2. */
 
   #define RCC_USART_TX_GPIO (RCC_GPIOA)
   #define GPIO_USART_TX_PORT (GPIOA)
-  #define GPIO_USART_TX_PIN (GPIO2) /* D1. */
-
-  #define RCC_LED_GPIO (RCC_GPIOA)
-  #define GPIO_LED_PORT (GPIOA)
-  #define GPIO_LED_PIN (GPIO5) /* D13. */
+  #define GPIO_USART_TX_PIN (GPIO2) /* ST-Link (Arduino-D1). */
 #elif defined(NUCLEO_F446RE)
   #define ADC_SIMPLE_TIME (ADC_SMPR_SMP_56CYC)
   #define RCC_ADC_GPIO (RCC_GPIOA)
   #define GPIO_ADC_PORT (GPIOA)
-  #define GPIO_ADC_A0_PIN (GPIO0) /* A0. */
-  #define GPIO_ADC_A1_PIN (GPIO1) /* A1. */
-  #define GPIO_ADC_A2_PIN (GPIO4) /* A2. */
+  #define GPIO_ADC_IN0_PIN (GPIO0) /* Arduino-A0. */
+  #define GPIO_ADC_IN1_PIN (GPIO1) /* Arduino-A1. */
+  #define GPIO_ADC_IN4_PIN (GPIO4) /* Arduino-A2. */
 
   #define RCC_USART_TX_GPIO (RCC_GPIOA)
   #define GPIO_USART_TX_PORT (GPIOA)
-  #define GPIO_USART_TX_PIN (GPIO2) /* D1. */
+  #define GPIO_USART_TX_PIN (GPIO2) /* ST-Link (Arduino-D1). */
   #define GPIO_USART_AF (GPIO_AF7)  /* Ref: Table-11 in DS10693. */
-
-  #define RCC_LED_GPIO (RCC_GPIOA)
-  #define GPIO_LED_PORT (GPIOA)
-  #define GPIO_LED_PIN (GPIO5) /* D13. */
 #else
   #error "STM32 board not defined."
 #endif
 
   static void rcc_setup(void);
   static void usart_setup(void);
-  static void led_setup(void);
   static void adc_setup(void);
-
-  static uint16_t get_adc_value(int channel);
   static void delay(uint32_t value);
 
 #ifdef __cplusplus
