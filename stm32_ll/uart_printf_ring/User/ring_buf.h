5abccfbe11d19e0592d193d54edee12080d99eb2
ziteh <honmonoh@gmail.com>
2024-05-25 21:57:38 +0800

    Add doc comment

--- stm32_ll/uart_printf_ring/User/ring_buf.h
+++ stm32_ll/uart_printf_ring/User/ring_buf.h
@@ -4,22 +4,39 @@
 /// @author ZiTe <honmonoh@gmail.com>
 /// @copyright SPDX-License-Identifier: MIT
 
-#ifndef RING_BUF_H
-#define RING_BUF_H
+#pragma once
 
 #include <stdint.h>
 
-/// @brief FIFO ring buffer. The `size` MUST be a power of 2.
+/// @brief FIFO ring buffer.
+/// @note The `size` must be power of 2.
 typedef struct {
-    uint8_t *const buffer;
-    const uint16_t size;
-    uint16_t in;
-    uint16_t out;
+    uint8_t *const buffer; // Pointer to the buffer
+    const uint16_t size;   // Size of the buffer, must be power of 2
+    uint16_t in;           // Input index
+    uint16_t out;          // Output index
 } ring_buf_t;
 
+/// @brief Puts data data into the ring buffer.
+/// @param ring Pointer to the ring buffer.
+/// @param data Pointer to the data to put.
+/// @param len Length of the data.
+/// @return The number actually put into the buffer.
 uint16_t ring_put(ring_buf_t *ring, const uint8_t *data, uint16_t len);
-uint16_t ring_get(ring_buf_t *ring, uint8_t *data, uint16_t len);
-uint8_t ring_get_1byte(ring_buf_t *ring);
-uint8_t ring_is_empty(const ring_buf_t *ring);
 
-#endif
+/// @brief Gets data from the ring buffer.
+/// @param ring Pointer to the ring buffer.
+/// @param data Pointer to the buffer to store the data.
+/// @param len Max length of the data to get.
+/// @return The number actually read from the buffer.
+uint16_t ring_get(ring_buf_t *ring, uint8_t *data, uint16_t len);
+
+/// @brief Gets one byte from the ring buffer.
+/// @param ring Pointer to the ring buffer.
+/// @return The byte read from the buffer.
+uint8_t ring_get_1byte(ring_buf_t *ring);
+
+/// @brief Checks if the ring buffer is empty.
+/// @param ring Pointer to the ring buffer.
+/// @return 1 if the buffer is empty, 0 otherwise.
+uint8_t ring_is_empty(const ring_buf_t *ring);
